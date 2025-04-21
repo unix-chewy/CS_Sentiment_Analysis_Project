c5595a05efb98900d477cc192a02f1fd29717196
unix-chewy <sunga.joney22@gmail.com>
2025-04-21 11:10:01 +0800

    1. Dummy Commit

--- app/views/admin/admin_homepage.php
+++ app/views/admin/admin_homepage.php
@@ -4,8 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../../public/assets/css/user-homepage.css">
 </head>
 <body>
-    <p>Admin</p>
+
+    <header class="header sticky-header">
+    <div class="navbar-wrapper">
+      <nav class="navbar">
+
+        <!-- Left section -->
+        <div class="left-section">
+          <a class="nav-link" href="#">Seller Centre</a>
+          <a class="nav-link" href="#">Start Selling</a>
+          <a class="nav-link" href="#">Download</a>
+          <span class="social-media-label">Follow us on</span>
+          <div class="social-media-links">
+            <a class="social-media-icon facebook" href="#" title="Facebook"></a>
+            <a class="social-media-icon instagram" href="#" title="Instagram"></a>
+          </div>
+        </div>
+
+        <div class="navbar-spacer"></div>
+
+        <!-- Right section links -->
+        <ul class="right-section">
+          <li class="nav-item notifications">
+            <a href="#" class="nav-link">Notifications</a>
+          </li>
+          <li class="nav-item help">
+            <a href="#" class="nav-link">Help</a>
+          </li>
+          <li class="nav-item language">
+            <a href="#" class="nav-link">English</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <!-- Logo + Search -->
+    <div class="logo-search-wrapper">
+      <div class="logo-search-container">
+        <div class="logo"></div>
+
+        <div class="search-bar">
+          <div class="search-input-container">
+            <input type="text" class="search-input" placeholder="Search...">
+            <button class="search-button"></button>
+          </div>
+        </div>
+
+
+        <div class="cart-wrapper">
+          <div class="cart-icon-container">
+            <a href="#" class="cart-icon"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
 </body>
 </html>
