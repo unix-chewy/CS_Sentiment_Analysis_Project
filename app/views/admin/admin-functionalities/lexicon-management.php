a625bcfa4a1289dc36770413d89b7f54ab6b60ac
Kyle Naguit <naguitkylechester@gmail.com>
2025-05-11 16:07:54 +0800

    Added home button admin sidebar

--- app/views/admin/admin-functionalities/lexicon-management.php
+++ app/views/admin/admin-functionalities/lexicon-management.php
@@ -80,6 +80,12 @@ include '../../../controllers/admin/admin-session.php';
          </div>
          <div class="sidebar-menu">
             <ul class="nav flex-column">
+               <!-- Home -->
+               <li class="nav-item">
+                  <a class="nav-link" href="../admin_homepage.php">
+                  <i class="bi bi-house"></i> Home
+                  </a>
+               </li>
                <!-- Product Management -->
                <li class="nav-item">
                   <a class="nav-link collapsed" href="product-management.php">
